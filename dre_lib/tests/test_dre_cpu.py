f75b83d5c78f83ca3283ed77f0732f58d104cc08
Andre Oporto <andreoporto@gmail.com>
2021-01-27 14:44:02 -0500

    add unit test for dre_cpu

---
+++ dre_lib/tests/test_dre_cpu.py
@@ -0,0 +1,24 @@
+import unittest
+from dre_lib.dre_cpu import CpuCount
+
+
+class CpuCountTestCase(unittest.TestCase):
+
+    def test_cpu_count_success(self):
+        cpu_count = CpuCount()
+        cpu_count._cpu_count = 8  # override for testing
+
+        self.assertEqual(8, cpu_count.total_count(), 'total cpu count should be 8')
+        self.assertEqual(7, cpu_count.adjusted_count(), 'adjusted cpu count should be 7')
+        self.assertEqual(5, cpu_count.adjusted_count(3), 'adjusted cpu count should be 5')
+        self.assertEqual(6, cpu_count.adjusted_count_by_percent(), 'adjusted cpu count should be 6')
+        self.assertEqual(2, cpu_count.adjusted_count_by_percent(0.75), 'adjusted cpu count should be 2')
+
+    @staticmethod
+    def test_print_stats_success():
+        cpu_count = CpuCount()
+        cpu_count.print_stats()
+
+
+if __name__ == '__main__':
+    unittest.main()
