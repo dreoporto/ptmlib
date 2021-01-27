75eb942c7348536fc9d2c98dfb2e6aaa7740b6d0
Andre Oporto <andreoporto@gmail.com>
2021-01-27 17:41:59 -0500

    add cpu_count test for excluding all processors

--- dre_lib/tests/test_dre_cpu.py
+++ dre_lib/tests/test_dre_cpu.py
@@ -14,6 +14,12 @@ class CpuCountTestCase(unittest.TestCase):
         self.assertEqual(6, cpu_count.adjusted_count_by_percent(), 'adjusted cpu count should be 6')
         self.assertEqual(2, cpu_count.adjusted_count_by_percent(0.75), 'adjusted cpu count should be 2')
 
+    def test_cpu_exclude_all_success(self):
+        cpu_count = CpuCount()
+        total_cpu_count = cpu_count.total_count()
+        self.assertEqual(1, cpu_count.adjusted_count(total_cpu_count), 'adjusted cpu count should be 1 at a minimum')
+        self.assertEqual(1, cpu_count.adjusted_count_by_percent(1.0), 'adjusted cpu count should be 1 at a minimum')
+
     @staticmethod
     def test_print_stats_success():
         cpu_count = CpuCount()
