f12598372bf248a30abd7933382788555f52e9be
Andre Oporto <andreoporto@gmail.com>
2021-02-01 14:59:20 -0500

    add AlertSounds class; add type annotations; change wikimedia sound for colab

--- ptmlib/time.py
+++ ptmlib/time.py
@@ -21,22 +21,26 @@ def get_time_string():
     return time.strftime("%Y%m%d-%H%M%S")
 
 
+class AlertSounds:
+    BEE5: str = 'media/bee5.mp3'
+    DORE: str = 'media/dore.mp3'
+
+
 class Stopwatch:
 
-    sounds_bee5 = 'media/bee5.mp3'
-    sounds_dore = 'media/dore.mp3'
-    default_colab_sound_url = 'https://upload.wikimedia.org/wikipedia/commons/0/05/Beep-09.ogg'
+    default_colab_sound_url: str = 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Bee5th.ogg'
 
     def __init__(self):
         self._start_time = None
 
-    def start(self):
+    def start(self) -> None:
         # display actual start time: useful for long-running tasks
         print("Start Time:", time.ctime())
 
         self._start_time = time.perf_counter()
 
-    def stop(self, silent=False, sound_path=sounds_bee5, colab_sound_url=default_colab_sound_url):
+    def stop(self, silent: bool = False, sound_path: str = AlertSounds.BEE5,
+             colab_sound_url: str = default_colab_sound_url) -> None:
 
         if self._start_time is None:
             raise ValueError('start time must be set by calling start() before stop()')
@@ -53,7 +57,8 @@ class Stopwatch:
         if not silent:
             self._alert_finished(sound_path, colab_sound_url)
 
-    def _alert_finished(self, sound_path, colab_sound_url):
+    @staticmethod
+    def _alert_finished(sound_path: str, colab_sound_url: str) -> None:
 
         dirname = os.path.dirname(__file__)
 
@@ -61,10 +66,11 @@ class Stopwatch:
         if os.path.exists(sound_path):
             pass  # no changes needed
         elif os.path.exists(os.path.join(dirname, sound_path)):
+            # relative path
             sound_path = os.path.join(dirname, sound_path)
-        elif os.path.exists(os.path.join(dirname, self.sounds_bee5)):
+        elif os.path.exists(os.path.join(dirname, str(AlertSounds.BEE5))):
             # switch back to default
-            sound_path = os.path.join(dirname, self.sounds_bee5)
+            sound_path = os.path.join(dirname, str(AlertSounds.BEE5))
         else:
             sound_path = None
 
