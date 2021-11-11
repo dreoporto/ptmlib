a2ca73e5c7190fec447e2c01c33954fe2d759d81
Andre Oporto <andreoporto@gmail.com>
2021-11-11 07:33:05 -0500

    update setup.py version to 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ptmlib",
-    version="0.1.2",
+    version="0.1.3",
     author="Andre Oporto",
     author_email="opor7ae@pendragonai.com",
     description="Pendragon Tools for Machine Learning",
