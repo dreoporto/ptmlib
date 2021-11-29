0009a4202d2e793d5244eff46a2d8a81592a6ecf
Andre Oporto <andreoporto@gmail.com>
2021-11-29 07:28:30 -0500

    refactor load_or_fit_model; use matplotlib for image display; add example script

---
+++ ptmlib/examples/computer_vision_caching.py
@@ -0,0 +1,121 @@
+# THIS CODE IS A MODULARIZED AND UPDATED VERSION OF CODE FROM THE "DEEPLEARNING.AI TENSORFLOW DEVELOPER" COURSE
+# SOURCE:
+# https://github.com/lmoroney/dlaicourse/blob/master/Course%201%20-%20Part%204%20-%20Lesson%202%20-%20Notebook.ipynb
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ptmlib.time import Stopwatch
+import ptmlib.charts as pch
+import ptmlib.model_tools as modt
+
+
+class MyCallback(keras.callbacks.Callback):
+
+    def __init__(self, target):
+        super().__init__()
+        self.target = target
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        if logs is None:
+            logs = {}
+        if logs.get("accuracy") > self.target:
+            print(f"\nReached {self.target * 100}% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+def print_diagnostics() -> None:
+    print('TF VERSION:', tf.__version__)
+    print('KERAS VERSION:', keras.__version__)
+
+
+def get_data():
+    mnist = keras.datasets.fashion_mnist
+    (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+    np.set_printoptions(linewidth=200)
+    plt.imshow(training_images[0])
+    plt.show()
+
+    print(training_labels[0])
+
+    # normalize image data to values between 0 and 1
+    training_images = training_images / 255.0
+    test_images = test_images / 255.0
+
+    return (training_images, training_labels), (test_images, test_labels)
+
+
+def get_model() -> keras.models.Sequential:
+    model = keras.models.Sequential([
+        layers.Flatten(input_shape=(28, 28)),
+        layers.Dropout(0.2),
+        layers.Dense(512, activation=tf.nn.relu),
+        layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.summary()
+
+    model.compile(
+        optimizer=tf.optimizers.Adam(),
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+def main():
+
+    # HYPER PARAMS
+    hp_epochs = 50
+    hp_target = 0.91
+    hp_validation_split = 0.2
+
+    print_diagnostics()
+
+    (training_images, training_labels), (test_images, test_labels) = get_data()
+
+    model = get_model()
+
+    early_callback = MyCallback(target=hp_target)
+
+    # TODO AEO clean up
+    # stopwatch.start()
+    #
+    # history = model.fit(
+    #     training_images,
+    #     training_labels,
+    #     validation_split=hp_validation_split,
+    #     epochs=hp_epochs,
+    #     callbacks=[early_callback]
+    # )
+    #
+    # stopwatch.stop()
+
+    fit_model_function_with_callback = lambda my_model, train_set, test_set, epochs: my_model.fit(
+        training_images, training_labels, epochs=epochs, callbacks=[early_callback],
+        validation_split=hp_validation_split)
+
+    model, history = modt.load_or_fit_model(model, "computer_vision_1", train_set=None, epochs=hp_epochs,
+                                            fit_model_function=fit_model_function_with_callback, metrics=["accuracy"])
+
+    model.evaluate(test_images, test_labels)
+
+    classifications = model.predict(test_images)
+    print(classifications[0])
+    print(test_labels[0])
+    print(max(classifications[0]))
+
+    # TODO AEO clean up
+    # pch.show_history_chart(history, "accuracy", save_fig_enabled=hp_save_fig_enabled)
+    # pch.show_history_chart(history, "loss", save_fig_enabled=hp_save_fig_enabled)
+
+
+if __name__ == '__main__':
+    main()
--- ptmlib/model_tools.py
+++ ptmlib/model_tools.py
@@ -1,8 +1,8 @@
 import os
-from typing import Any
+from typing import Any, List
 
-from IPython.core.display import display
-from IPython.display import Image
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 from tensorflow import keras
 
 import ptmlib.charts as pch
@@ -13,29 +13,53 @@ def default_load_model_function(model_file_name: str):
     return keras.models.load_model(f'{model_file_name}.h5')
 
 
-def default_fit_model_function(model: Any, train_set: Any, test_set: Any, epochs: int):
+# TODO AEO - change train_set to x, y; change test_set to validation_data
+
+def _default_fit_model_function(model: Any, train_set: Any, test_set: Any = None, epochs: int = 1):
     return model.fit(train_set, validation_data=test_set, epochs=epochs)
 
 
-def load_or_fit_model(model, model_file_name, train_set, test_set=None, epochs=1,
+def load_or_fit_model(model: Any, model_file_name: str, train_set: Any, test_set: Any = None, epochs: int = 1,
+                      metrics: List[str] = None, images_enabled=True,
                       load_model_function=default_load_model_function,
-                      fit_model_function=default_fit_model_function):
+                      fit_model_function=_default_fit_model_function):
     history = None
 
     if os.path.exists(f'{model_file_name}.h5'):
         model = load_model_function(model_file_name)
-        if os.path.exists(f'accuracy-{model_file_name}.png'):
-            display(Image(filename=f'accuracy-{model_file_name}.png'))
-        if os.path.exists(f'loss-{model_file_name}.png'):
-            display(Image(filename=f'loss-{model_file_name}.png'))
+        if images_enabled:
+            _show_saved_images(metrics, model_file_name)
     else:
         stopwatch = Stopwatch()
         stopwatch.start()
         history = fit_model_function(model, train_set, test_set, epochs)
         stopwatch.stop()
         model.save(f'{model_file_name}.h5')
-        # TODO AEO add error handling to show_history_chart in case 'accuracy' is not available
-        pch.show_history_chart(history, "accuracy", save_fig_enabled=True, file_name_suffix=model_file_name)
-        pch.show_history_chart(history, "loss", save_fig_enabled=True, file_name_suffix=model_file_name)
+        if images_enabled:
+            _show_new_images(history, model_file_name, metrics)
 
     return model, history
+
+
+def _show_new_images(history: Any, model_file_name: str, metrics: List[str]):
+    # TODO AEO add error handling to show_history_chart in case 'accuracy' is not available
+    if metrics is not None:
+        for metric in metrics:
+            pch.show_history_chart(history, metric, save_fig_enabled=True, file_name_suffix=model_file_name)
+    pch.show_history_chart(history, "loss", save_fig_enabled=True, file_name_suffix=model_file_name)
+
+
+def _show_saved_images(metrics: List[str], model_file_name: str):
+    if metrics is not None:
+        for metric in metrics:
+            if os.path.exists(f'{metric}-{model_file_name}.png'):
+                _show_saved_image(f'{metric}-{model_file_name}.png')
+    if os.path.exists(f'loss-{model_file_name}.png'):
+        _show_saved_image(f'loss-{model_file_name}.png')
+
+
+def _show_saved_image(filename: str):
+    image_data = mpimg.imread(filename)
+    plt.axis('off')
+    plt.imshow(image_data)
+    plt.show()
